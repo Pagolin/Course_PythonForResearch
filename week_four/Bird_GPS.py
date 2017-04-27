60ba803815ae5965c0941762eeccdb8d91c65a64
Lisza <lisza.zeidler@mailbox.tu-dresden.de>
2017-04-27 14:54:01 +0200

    NAN handling in pyplots

--- week_four/Bird_GPS.py
+++ week_four/Bird_GPS.py
@@ -28,3 +28,33 @@ plt.ylabel("Latitude")
 plt.legend(loc="lower right")
 #plt.show()
 plt.savefig("./bird_plots/uncorrected_paths.pdf")
+
+"""Work with speed data (column 'speed_2d') of the birds to exemplify the handling of NAN's in the data"""
+
+speed = bird_data.speed_2d[Eric_index]
+#pyplot can't handle NAN#s -> find them with numpy
+np.isnan(speed).any()
+#or how many
+np.sum(np.isnan(speed))
+#or true or false for all elements in speed
+ind = np.isnan(speed)
+#take ~ind => Komplement of ind vektor where NAN's in speed are False
+complement = ~ind
+plt.figure(figsize= (12,4))
+plt.subplot(221)
+plt.hist(speed[complement], bins=np.linspace(0, 30, 20), normed=True)
+plt.xlabel("2D speed in m/s")
+plt.ylabel("Frequency")
+plt.subplot(222)
+plt.hist(speed[complement], bins=np.linspace(0, 30, 20), normed=False)
+plt.xlabel("2D speed in m/s")
+plt.ylabel("Frequency non-normed")
+plt.savefig("./bird_plots/speed_hist_Eric.pdf")
+
+#pandas irgnores NAN's when plotting data
+
+plt.figure()
+bird_data.speed_2d.plot(kind='hist', range = [0,30])
+plt.xlabel("2D speed in m/s")
+plt.ylabel("Frequency")
+plt.savefig("./bird_plots/pandas_hist_Eric.pdf")
