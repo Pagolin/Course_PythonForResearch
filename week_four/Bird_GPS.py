d0d20e24d9be69f258ec75529a645dfdba3babef
Lisza <lisza.zeidler@mailbox.tu-dresden.de>
2017-04-28 17:03:07 +0200

    Handling timestamps in bird_data

--- week_four/Bird_GPS.py
+++ week_four/Bird_GPS.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 bird_data = pd.read_csv("./bird_data/bird_tracking.csv")
 
-print(bird_data.info())
+#print(bird_data.info())
 
 """draw a first plot of longitude vs latitude of bird flights, not yet taking into account the distortions caused by the fact, the plot is 2D-> flat,  while flight coordinates are measured over a speric ground"""
 
@@ -13,9 +13,6 @@ print(bird_data.info())
 Eric_index = bird_data.bird_name =="Eric"
 x, y = bird_data.longitude[Eric_index], bird_data.latitude[Eric_index]
 #distorted flight/ migration path of bird Eric
-
-
-
 
 bird_names = pd.unique(bird_data.bird_name)
 plt.figure(figsize=(7,7))
@@ -58,3 +55,37 @@ bird_data.speed_2d.plot(kind='hist', range = [0,30])
 plt.xlabel("2D speed in m/s")
 plt.ylabel("Frequency")
 plt.savefig("./bird_plots/pandas_hist_Eric.pdf")
+
+"""Work with timestamp data 
+Timestamps are often (as in this case) given as strings. Pythons datetime libraray 
+allows converting them into datetime objects
+"""
+import datetime
+times_string = bird_data.date_time
+
+timestamps = []
+# datetimes function strptime(string, format_string) turns the first string argument into a datetime object, according to a format specified in it's second, format string argument
+
+for k in range(len(bird_data)):
+    timestamps.append(datetime.datetime.strptime(bird_data.date_time.iloc[k][:-3], "%Y-%m-%d %H:%M:%S"))
+#print(timestamps[0:3])
+
+# append the timestamps as a new column to bird_data
+bird_data["timestamp"] = pd.Series(timestamps, index=bird_data.index)
+#print(bird_data.head())
+
+# Calculate elapsed time since the first observation for the data of Eric
+times = bird_data.timestamp[Eric_index]
+elapsed_times = [time - times[0] for time in times]
+
+# To get the times in a particular unit i.e. hours, days etc. divide timestamp object by a timedelta object
+# As elapsed_times is a numpy array division is done elementwise without list comprehension
+
+elapsed_days = np.array(elapsed_times)/datetime.timedelta(days=1)
+print(elapsed_times[0:3])
+print(elapsed_days[0:3])
+plt.plot(elapsed_days)
+plt.xlabel("Observation")
+plt.ylabel("Elapsed time in days")
+plt.savefig("./bird_plots/flight_times_Eric.pdf")
+#TODO: Explore the originin of the strange blue line distoring the plot
