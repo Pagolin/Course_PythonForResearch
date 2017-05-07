170f1437dec4854caedca51f00931c798794f758
Lisza <lisza.zeidler@mailbox.tu-dresden.de>
2017-05-07 08:57:27 +0200

    Last lecture on Networkx, End of Course

--- week_four/HWNetworks.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-
-columns = ["village", "adjmatrix_key", "pid", "hhid", "resp_id", "resp_gend"]
--- week_four/NetworkAnalysisBasics.py
+++ week_four/NetworkAnalysisBasics.py
@@ -124,9 +124,33 @@ plot_degree_distribution(G2)
 plt.show()
 #Conclusion => ER graphs are not an appropriate approximation to social network graphs
 
+"""
+Find connected components in graphs 
+Generator functions: Networkx function connected_component_subgraph() doesn't create a data object but a generator object, that can be used to create a series of objects using the next()  function (like a "productive iterator").
+"""
+generator = nx.connected_component_subgraphs(G1)
+component = generator.__next__()
+nr_of_nodes = len(generator.__next__())
+print("Produced a subgraph with %d Nodes, next subgraph will have %d Nodes" %(component.number_of_nodes(), nr_of_nodes))
 
+#Find the largest conected component
 
+gen = nx.connected_component_subgraphs(G1)
+G1_largest_connected_component = max(nx.connected_component_subgraphs(G1), key = len)
+G2_largest_connected_component= max(nx.connected_component_subgraphs(G2), key = len)
+print("Largest components have %d and %d Nodes in G1 and G2, respectively" %(len(G1_largest_connected_component), len(G2_largest_connected_component)))
 
+"""
+Visualize components
+By default nx.draw plots the graphs stochastically, altering the appearance of each graph slightly in every single call.
+However general tendencies, as the ovious partition of G2s largest subset into two, connected subgroups are present in every vizualisation
+"""
+plt.figure()
+nx.draw(G1_largest_connected_component, node_color= "blue", edge_color= "darkgrey", nodesize=3)
+plt.savefig("./network_plots/largest_component_village1.pdf")
+plt.figure()
+nx.draw(G2_largest_connected_component, node_color= "blue", edge_color= "darkgrey", nodesize=3)
+plt.savefig("./network_plots/largest_component_village2.pdf")
 
 
 
